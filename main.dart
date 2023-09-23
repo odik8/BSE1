7e4e28f798813f77621acd71448a451f38277ad6
Odyssey Meltonyan <odissey.meltonyan@gmail.com>
2023-09-23 18:36:11 +0300

    Commit 4

--- main.dart
+++ main.dart
@@ -4,7 +4,9 @@ import "dart:math";
 void main() {
   print(sum(2, 2));
   print(randomBoll());
+  print(randomDouble());
 }
 
 sum(int a, int b) => a + b;
 randomBoll() => Random().nextBool();
+randomDouble() => Random().nextDouble();
