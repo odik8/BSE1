3ef04a4a288121e5ff0a9773bbd95174e0475b63
Odyssey Meltonyan <odissey.meltonyan@gmail.com>
2023-09-23 18:42:05 +0300

    Commit 5

--- main.dart
+++ main.dart
@@ -5,8 +5,15 @@ void main() {
   print(sum(2, 2));
   print(randomBoll());
   print(randomDouble());
+  print(randomString(10));
 }
 
 sum(int a, int b) => a + b;
 randomBoll() => Random().nextBool();
 randomDouble() => Random().nextDouble();
+randomString(int length) {
+  final random = Random();
+  const charset = '1234567890qwertyuiopasdfghjklmnbvcxz';
+  return List.generate(
+      length, (index) => charset[random.nextInt(charset.length)]).join();
+}
